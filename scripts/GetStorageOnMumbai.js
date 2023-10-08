92a74726ff535e2e98ebb7ef059eeec496211722
Taneristique <taberandwords@hotmail.com>
2023-10-08 18:48:36 +0300

    Access to the value of number by calling eth.getStorageAt() on Mumbai testnet

---
+++ scripts/GetStorageOnMumbai.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const ethers = require("ethers");
+let provider=null;
+async function accessNumber(){
+    provider = new ethers.providers.JsonRpcProvider(`https://polygon-mumbai.infura.io/v3/${process.env.ApiKey}`);
+    console.log(await provider.getStorageAt("0x1d7C29a84F3472A8335ef5715770E9bDAB584ff6",0));
+}
+accessNumber();
